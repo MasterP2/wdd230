fe42e62e0497e0de53e26e0935ad8e534dddf0b5
MasterP2 <arikpopeace98@gmail.com>
2023-01-14 09:34:34 -0500

    week02

--- week02/scripts/Responsive-Menu.js
+++ week02/scripts/Responsive-Menu.js
@@ -1,3 +1,9 @@
+document.getElementById("copyright").innerHTML = paragraph
+
+var date = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = "Last Modification: " + date;
+
 const menubutton = document.querySelector("#menu-button");
 const menuitems = document.querySelectorAll(".menu-item");
 // menuitems will be a Node List of the list items
