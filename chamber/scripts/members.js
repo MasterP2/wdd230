bb8043ad904d7005bd679b81dfcf4a0f4b9ce30b
MasterP2 <arikpopeace98@gmail.com>
2023-02-11 21:28:31 -0500

    Update

---
+++ chamber/scripts/members.js
@@ -0,0 +1,44 @@
+
+const url = 'https://peace-23.github.io/wdd230/chamber/data/members.json';
+
+const cards = document.querySelector('.cards');
+
+async function GetMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+GetMemberData();
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h3');
+        let icon = document.createElement('img');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        
+
+        fullName.textContent = `${member.name}`;
+        address.textContent = `${member.address}`;
+        phone.textContent = `${member.phone}`;
+        website.innerHTML = `${member.url}`;
+        website.setAttribute('href', `${member.url}`);
+        
+        icon.setAttribute('src', member.icon);
+        icon.setAttribute('alt', `${member.name}`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '200');
+        icon.setAttribute('height', '100');
+
+        card.appendChild(icon);
+        card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        
+
+        cards.appendChild(card);
+    });
+}
